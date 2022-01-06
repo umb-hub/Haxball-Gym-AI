0a9c7ccd479aa69755eff640a0473dd0c75df42a
qu_by <umberto.barbato@icloud.com>
2022-01-06 15:55:09 +0100

    init HaxballEnv

--- gym_haxball/envs/HaxballEnv.py
+++ gym_haxball/envs/HaxballEnv.py
@@ -5,3 +5,18 @@ try:
     import hfo_py
 except ImportError as e:
     raise error.DependencyNotInstalled("{}. (HINT: you can install HFO dependencies with 'pip install gym[soccer].)'".format(e))
+
+
+class HaxballEnv(Env):
+    
+    def __init__(self):
+        pass
+    
+    def step(self):
+        pass
+
+    def render(self):
+        pass
+
+    def reset(self):
+        pass
